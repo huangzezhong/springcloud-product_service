1d782f600964fdced8a616a69e5cf9f4e4192293
huangzezhong <huangzezhong111@sina.com>
2019-03-04 17:49:56 +0800

    springcloud商品服务项目

--- src/main/java/com/gz/ProductServiceApplication.java
+++ src/main/java/com/gz/ProductServiceApplication.java
@@ -3,6 +3,9 @@ package com.gz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 商品服务启动类
+ */
 @SpringBootApplication
 public class ProductServiceApplication {
 
