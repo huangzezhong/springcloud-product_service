c6ec0f0cfd5933bda0ded8ffc3eeeb0d55e27fa4
huangzezhong <huangzezhong111@sina.com>
2019-03-04 17:54:10 +0800

    增加注释

--- src/main/java/com/gz/ProductServiceApplication.java
+++ src/main/java/com/gz/ProductServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 商品服务启动
+ * 商品服务启动类
  */
 @SpringBootApplication
 public class ProductServiceApplication {
